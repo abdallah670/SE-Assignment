1ec6da3e43b13c7d6cae84d76a8bc85a64bc22e1
Abdullah MO <abdallahmofe@hotmail.com>
2025-05-08 22:10:43 +0300

    Add project files.

---
+++ virtualUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Bugeting
+{
+    internal class virtualUI
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
